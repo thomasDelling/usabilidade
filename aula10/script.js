8173ed9bdf27e209703f0db6cbf6e66775b6001a
thomasDelling <thomasmaxdelling6@gmail.com>
2025-04-10 11:28:23 -0300

    aprendendo mais

---
+++ aula10/script.js
@@ -0,0 +1,21 @@
+console.log("fodase");
+
+
+
+document.getElementById("btn1").addEventListener("click", () => {
+    console.log("criou botao");
+    let valorSepara = parseInt(document.getElementById("cxaSepara").value)
+    console.log("valorSepara", valorSepara);
+    let valorPalpite = parseInt(valorPalpite);
+    valorPalpite = parseInt(valorPalpite);
+    console.log("valor palpite", valorPalpite);
+    let elem = document.getElementById("msg");
+    console.log(elem);
+ 
+    
+})
+
+
+
+
+
