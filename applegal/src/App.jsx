5de65bd7b595ec73bd2b2661779316147c01001b
thomasDelling <thomasmaxdelling6@gmail.com>
2025-04-17 11:44:32 -0300

    introduzindo react

---
+++ applegal/src/App.jsx
@@ -0,0 +1,13 @@
+import Caixa from "./src/assents/App";
+function App() {
+
+  return (
+    <>
+      oi gente
+      <button>clique aqui</button>
+      <Caixa />
+    </>
+  )
+}
+
+export default App;
---
+++ applegal/src/Caixa.jsx
@@ -0,0 +1,9 @@
+
+export default function Caixa(){
+
+    return(
+        <>
+        Caixa
+        </>
+    )
+}
